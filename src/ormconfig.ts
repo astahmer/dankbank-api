7afefc76131cac697765908657533c2ca6e4293a
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2019-09-02 19:31:31 +0200

    refacto: get rid of sqlite3 & install mysql/sharp

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,9 +1,11 @@
 export const TypeORMConfig = {
-    type: "sqlite",
-    database: "database.sqlite",
     synchronize: true,
     logging: ["query"],
     logger: "file",
     migrations: ["src/migration/**/*.ts"],
     subscribers: ["src/subscribers/**/*.ts"],
+    cli: {
+        entitiesDir: "src/entity",
+        migrationsDir: "src/migration",
+    },
 };
